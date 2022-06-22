fc728fa6bb1a6746e4d10b38a662b70f5c808d64
Leonardo Muñoz <contacto@pctester.cl>
2022-06-22 13:44:28 -0400

    commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    /> -->
+
+    {{ clientes }}
+    <!-- <q-table
+      title="Clientes"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      dark
+      color="amber"
+    /> -->
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import { api } from "boot/axios";
+import { useQuasar } from "quasar";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    const $q = useQuasar();
+    const clientes = ref(null);
+
+    const loadData = () => {
+      api
+        .get("/clientes", {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjExMjIyMzM4LCJyb2xlIjoxLCJpYXQiOjE2NTU4Mjk1OTh9.7yf8gREybtZ_-G7hovkEHAoC6AXh29JlyvRpRPVmSyk",
+          },
+        })
+        .then((response) => {
+          clientes.value = response.data;
+          console.log(response.data);
+        })
+        .catch(() => {
+          $q.notify({
+            color: "negative",
+            position: "top",
+            message: "Loading failed",
+            icon: "report_problem",
+          });
+        });
+    };
+
+    loadData();
+
+    return {
+      clientes,
+    };
+  },
+});
+</script>
