abf87d77723c38eb45558b91a996fbe39e5d6eb5
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2022-07-14 18:29:59 -0400

    add server prod 2

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,11 +7,10 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-let port = process.env.PORT || 8000;
 let server =
   process.env.NODE_ENV == "production"
-    ? `https://otmaster.up.railway.app:${port}/api/v1/`
-    : `http://localhost:${port}/api/v1/`;
+    ? `https://otmaster.up.railway.app/api/v1/`
+    : `http://localhost:8000/api/v1/`;
 
 const api = axios.create({
   baseURL: server,
